588e022a683dd9c8aae9717b9a6be1a8048f5d8d
Kevin Hui <kevinhui98@gmail.com>
2023-01-05 20:10:31 -0500

    starting nav bar

---
+++ src/components/layout/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+
+const Layout = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Layout
