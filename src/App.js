545ce3f6a97a2da672d735f6cb84b69d75b90634
Kevin Hui <kevinhui98@gmail.com>
2023-02-01 23:19:56 -0500

    Revert "Contact"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="Contact" element={<Contact />} />
-        </Route>
+        <Route path="/" element={<Layout />} />
       </Routes>
     </>
   );
